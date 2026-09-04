981c3c574192ba86b81cc00fb89a6b20facd0230
YamiDV <yamivritra@gmail.com>
2026-09-04 02:01:41 -0500

    first

---
+++ src/test/java/com/cursos/api/springmodel/SpringModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.api.springmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
